629efc5038856972b084f2b533667c2241f1b490
Toygoon <toygoon@gmail.com>
2022-04-27 12:57:04 +0900

    FCFS wip

--- Schd/AlgsFCFS.cs
+++ Schd/AlgsFCFS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -21,34 +22,43 @@ namespace Schd
                 while(jobList.Count != 0)
                 {
                     Process frontJob = jobList.ElementAt(0);
+                    Debug.WriteLine("frontJob : " + jobList.ElementAt(0).processID);
                     if (frontJob.arriveTime == runTime)
                     {
                         readyQueue.Add(new ReadyQueueElement(frontJob.processID, frontJob.burstTime, 0));
+                        Debug.WriteLine("frontJob added to ready queue");
                         jobList.RemoveAt(0);
+                        Debug.WriteLine("frontJob has removed from jobList");
                     }
                     else
                     {
+                        Debug.WriteLine("frontJob - else break");
                         break;
                     }
                 }
 
                 if (currentProcess == 0)
                 {
+                    Debug.WriteLine("currentProcess is 0");
                     if (readyQueue.Count != 0)
                     {
                         ReadyQueueElement rq = readyQueue.ElementAt(0);
+                        Debug.WriteLine("Ready queue element : " + rq.processID);
                         resultList.Add(new Result(rq.processID, runTime, rq.burstTime, rq.waitingTime));
+                        Debug.WriteLine("Add to resultList; PID " + rq.processID + ", runTime "
+                            + runTime + ", bustTime " + rq.burstTime + ", waitingTime " + rq.waitingTime);
                         cpuDone = rq.burstTime;
                         cpuTime = 0;
                         currentProcess = rq.processID;
                         readyQueue.RemoveAt(0);
-
+                        Debug.WriteLine("cpuDone " + cpuDone + ", Removed process from readyQueue : " + rq.processID);
                     }
                 }
                 else
                 {
                     if (cpuTime == cpuDone)
                     {
+                        Debug.WriteLine("cpuTime : " + cpuTime + ", cpuDone : " + cpuDone);
                         currentProcess = 0;
                         continue;
                     }
@@ -57,9 +67,11 @@ namespace Schd
                 cpuTime++;
                 runTime++;
 
+                Debug.WriteLine("cpuTime : " + cpuTime + ", runTime : " + runTime);
                 for(int i = 0; i < readyQueue.Count; i++)
                 {
                     readyQueue.ElementAt(i).waitingTime++;
+                    Debug.WriteLine("PID " + readyQueue.ElementAt(i).processID + "'s waitingTime : " + readyQueue.ElementAt(i).waitingTime);
                 }
 
             } while (jobList.Count != 0 || readyQueue.Count != 0 || currentProcess != 0);
