2724a034e031162beda5edfa83001de253cec9f3
Toygoon <toygoon@gmail.com>
2022-04-28 23:41:49 +0900

    SRTF almost done

--- Schd/AlgsSRTF.cs
+++ Schd/AlgsSRTF.cs
@@ -9,15 +9,6 @@ namespace Schd
 {
     class AlgsSRTF
     {
-        public static int findNext(List<ProcessExt> processExt, int clock, int curExec)
-        {
-            if (processExt[curExec].burstTime <= 0)
-                processExt[curExec].isDone = true;
-
-
-            return curExec;
-        }
-
         public static List<Result> Run(List<Process> jobList, List<Result> resultList)
         {
             // Sort with; arriveTime, and burstTime
@@ -28,25 +19,77 @@ namespace Schd
                 else return x.burstTime.CompareTo(y.burstTime);
             });
 
-            int clock = 0, x = 0;
-            List<ProcessExt> processExt = new List<ProcessExt>();
+            Process exec = jobList[0];
+            int clock = 0, idx = 0, pid = 0, timeBursted = 0, beforepid = exec.processID;
+            List<Tuple<int, int>> waitingTime = new List<Tuple<int, int>>();
 
-            for (int i = 0; i < jobList.Count; i++)
-                processExt.Add(new ProcessExt(jobList[i].processID,
-                    jobList[i].arriveTime,
-                    jobList[i].burstTime,
-                    jobList[i].priority,
-                    jobList[i].burstTime,
-                    0,
-                    false));
+            for(int i=0; i<jobList.Count; i++)
+                waitingTime.Add(new Tuple<int, int>(jobList[i].processID, 0));
 
-            ProcessExt exec = processExt[0];
-
-            while (processExt.Count != 0)
+            while (jobList.Count != 0)
             {
-                clock++;
-                processExt[x].burstTime--;
+                pid = exec.processID;
+                idx = jobList.IndexOf(exec);
 
+                for (int i = 0; i < jobList.Count; i++)
+                    if (jobList[i].processID != pid && jobList[i].arriveTime <= clock)
+                        for(int j=0; j<waitingTime.Count; j++)
+                            if (waitingTime[j].Item1 == jobList[i].processID)
+                                waitingTime[j] = new Tuple<int, int>(jobList[i].processID, waitingTime[j].Item2 + 1);
+
+                clock++;
+                exec.burstTime--;
+                timeBursted++;
+
+                if (exec.burstTime <= 0)
+                {
+                    jobList.Remove(exec);
+
+                    if (jobList.Count == 0)
+                    {
+                        int tmp = 0;
+                        for (int i = 0; i < waitingTime.Count; i++)
+                            if (waitingTime[i].Item1 == beforepid)
+                            {
+                                tmp = i;
+                                break;
+                            }
+
+                        resultList.Add(new Result(exec.processID, clock - timeBursted, timeBursted, waitingTime[tmp].Item2));
+                        break;
+                    }
+
+                    exec = jobList[0];
+                    pid = exec.processID;
+                    idx = 0;
+                }
+
+                for (int i = 0; i < jobList.Count; i++)
+                {
+                    if (jobList[i].arriveTime <= clock
+                        && jobList[i].burstTime < jobList[idx].burstTime)
+                    {
+                        exec = jobList[i];
+                        pid = exec.processID;
+                        idx = i;
+                    }
+                }
+
+                if (beforepid != pid)
+                {
+                    int tmp = 0;
+                    for (int i = 0; i < waitingTime.Count; i++)
+                        if (waitingTime[i].Item1 == beforepid)
+                        {
+                            tmp = i;
+                            break;
+                        }
+
+                    resultList.Add(new Result(beforepid, clock - timeBursted, timeBursted, waitingTime[tmp].Item2));
+                    waitingTime[tmp] = new Tuple<int, int>(beforepid, 0);
+                    timeBursted = 0;
+                    beforepid = pid;
+                }
             }
 
             return resultList;
--- Schd/Process.cs
+++ Schd/Process.cs
@@ -11,14 +11,14 @@ namespace Schd
         public int arriveTime;
         public int burstTime;
         public int priority;
-   
+
         public Process(int processID, int arriveTime, int burstTime, int priority)
         {
             this.processID = processID;
             this.arriveTime = arriveTime;
             this.burstTime = burstTime;
             this.priority = priority;
-        } 
+        }
     }
 
     class ProcessExt : Process
