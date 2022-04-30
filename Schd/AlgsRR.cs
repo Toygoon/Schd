be99d0f0f7e4a1cd2e4566f7378377908f599282
Toygoon <toygoon@gmail.com>
2022-04-30 16:10:20 +0900

    RR renew wip

--- Schd/AlgsRR.cs
+++ Schd/AlgsRR.cs
@@ -25,13 +25,57 @@ namespace Schd
             resultList = new List<Result>();
 
             int clock = 0, timeBursted = 0;
+            bool checkExec;
 
-            List<ReadyQueueElement> readyQueue = new List<ReadyQueueElement>();
+            List<ReadyQueueElement> readyQueue;
+
+
             // Any process done for execution will be removed from jobList
             // Starting the CPU
             while (jobList.Count != 0)
             {
+                readyQueue = new List<ReadyQueueElement>();
+                checkExec = false;
 
+                // Checking for appropriate target processes existance
+                for (int i = 0; i < jobList.Count; i++)
+                    if (jobList[i].arriveTime <= clock)
+                    {
+                        readyQueue.Add(new ReadyQueueElement(jobList[i].processID, jobList[i].burstTime, 0));
+                        checkExec = true;
+                        break;
+                    }
+
+                // There's no process to execute
+                if (checkExec == false)
+                {
+                    clock++;
+                    continue;
+                }
+
+                ReadyQueueElement exec = null;
+                int cur = 0;
+
+                while (readyQueue.Count != 0)
+                {
+                    exec = readyQueue[cur];
+
+                    clock++;
+                    timeBursted++;
+                    exec.burstTime--;
+
+                    for (int i = 0; i<readyQueue.Count; i++)
+                        if (exec.processID != readyQueue[i].processID)
+                            readyQueue[i].waitingTime++;
+
+                    if (timeBursted == tq || exec.burstTime == 0)
+                    {
+                        // Todo; migrate readyqueue to joblist, add to resultlist
+                        Process tmp = jobList.Find(x => x.processID == exec.processID);
+                        readyQueue.Remove(exec);
+                        tmp.burstTime = exec.burstTime;
+                    }
+                }
             }
 
             return resultList;
